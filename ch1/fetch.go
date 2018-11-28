21d46f1df8f7e56ebf073867842014e853158727
yellowb <122517167@qq.com>
2018-11-28 14:28:23 +0800

    edit

---
+++ ch1/fetch.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	url := "http://www.baidu.com"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch error: %v\n", err)
+		os.Exit(1)
+	}
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch body error: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("%s\n", content)
+}
