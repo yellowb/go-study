d49b7f626666aa4f1022a1f869017d8bfc121467
yellowb <122517167@qq.com>
2018-12-10 17:10:27 +0800

    edit

--- ch8/testChannels.go
+++ ch8/testChannels.go
@@ -1,22 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"time"
 )
 
 func main() {
 	ch := make(chan int)
-	fmt.Printf("[Parent] started to invoke goroutine\n")
+	log.Printf("[Parent] started to invoke goroutine\n")
 	go childFunc(1, 2, ch)
-	fmt.Printf("[Parent] waiting for goroutine data...\n")
+	go childFunc(2, 3, ch)
+	log.Printf("[Parent] waiting for goroutine data...\n")
 	result := <-ch
-	fmt.Printf("[Parent] got result from goroutine with result = %d\n", result)
+	log.Printf("[Parent] got result from goroutine with result = %d\n", result)
+	result = <-ch
+	log.Printf("[Parent] got result from goroutine with result = %d\n", result)
 }
 
 func childFunc(a int, b int, ch chan int) {
-	fmt.Printf("[Child] got invoked with a = %d, b = %d\n", a, b)
-	time.Sleep(3 * time.Second)
-	fmt.Printf("[Child] calculation finished with result = %d\n", a+b)
+	log.Printf("[Child] got invoked with a = %d, b = %d\n", a, b)
+	time.Sleep(time.Duration(a+b) * time.Second)
+	log.Printf("[Child] calculation finished with result = %d\n", a+b)
 	ch <- a + b
 }
